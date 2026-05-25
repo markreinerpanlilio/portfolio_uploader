9b563ed613366fe98cac51571f265eeafb2295cf
markreinerpanlilio <markreinerpanlilio@gmail.com>
2026-05-25 22:40:32 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://cdwmqydqgvwpsifozmyn.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkd21xeWRxZ3Z3cHNpZm96bXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2ODEyNjMsImV4cCI6MjA5NTI1NzI2M30.KVlVkvnsiN11JABOXth1SoApGUoMxhyCM4w2e15ZnBg";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
